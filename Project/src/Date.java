659ae6f25b8be507543a751b27b2b4a74a3ef5ac
bwkopp <bwkopp@ucalgary.ca>
2017-04-02 16:51:42 -0600

    Added Date
    date class added

---
+++ Project/src/Date.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Brendon
+ * Stores a date
+ */
+public class Date {
+private Integer day;
+private Integer month;
+private Integer year;
+
+public Date(Integer d, Integer m, Integer y)
+{
+	day = d;
+	month = m;
+	year = y;
+}
+}

9663650b9b41ea42e1cf080320ae2c03d68f3478
bwkopp <bwkopp@ucalgary.ca>
2017-04-02 17:00:54 -0600

    Added getters and setters

--- Project/src/Date.java
+++ Project/src/Date.java
@@ -14,4 +14,55 @@ public Date(Integer d, Integer m, Integer y)
 	month = m;
 	year = y;
 }
+
+public void setDay(Integer d)
+{
+	if (d > 0 && d < 32)
+	{
+		day = d;
+	}
+	else
+	{
+		throw new java.lang.Error("day out of bounds");
+	}
+}
+
+public void setMonth(Integer m)
+{
+	if (m > 0 && m < 13)
+	{
+		month = m;
+	}
+	else
+	{
+		throw new java.lang.Error("month out of bounds");
+	}
+}
+
+public void setYear(Integer y)
+{
+	if (y > 2000 && y < 9999)
+	{
+		year = y;
+	}
+	else
+	{
+		throw new java.lang.Error("year out of bounds");
+	}
+}
+
+public Integer getDay()
+{
+	return day;
+}
+
+public Integer getMonth()
+{
+	return month;
+}
+
+public Integer getYear()
+{
+	return year;
+}
 }

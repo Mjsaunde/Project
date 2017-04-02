dd4ceebf0f14b7ce385f44f8d3c7339a597e5fd2
Mark <jamie_4000@hotmail.com>
2017-04-02 17:10:38 -0600

    Added Constructor to Passenger

--- Project/src/Passenger.java
+++ Project/src/Passenger.java
@@ -3,4 +3,10 @@ public class Passenger {
 String firstName = "";
 String lastName = "";
 Date birthday ;
+Passenger(String f, String l, Date b){
+	this.firstName = f;
+	this.lastName = l;
+	this.birthday = b;
+}
+Passenger(){}
 }

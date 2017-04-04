c7c56b22c412c6ed4e9c6b4016c23998fab2e6bd
bwkopp <bwkopp@ucalgary.ca>
2017-04-04 15:23:23 -0600

    add flight GUI
    admin add flight gui adapted fro flightGUI

--- Project/src/AddFlightGUI.java
+++ Project/src/AddFlightGUI.java
@@ -6,7 +6,7 @@ import javax.swing.*;
  * @author Brendon
  * 
  */
-public class FlightGUI extends JFrame {
+public class AddFlightGUI extends JFrame {
 
 	/**
 	 * 
@@ -18,25 +18,19 @@ public class FlightGUI extends JFrame {
 	JTextField txtFlightDest;
 	JTextField txtFlightNum;
 	JTextField txtFlightDur;
-	JTextField txtPassName;
-	JTextField txtPassLName;
-	JTextField txtPassBday;
 	JLabel lblFlightDate;
 	JLabel lblFlightTime;
 	JLabel lblFlightSrc;
 	JLabel lblFlightDest;
 	JLabel lblFlightNum;
 	JLabel lblFlightDur;
-	JLabel lblPassName;
-	JLabel lblPassLName;
-	JLabel lblPassBday;
-	JButton btnFlightBook;
+	JButton btnAddFlight;
 	JButton btnCancel;
 	
 	Container c;
 	
 	
-	public FlightGUI()
+	public AddFlightGUI()
 	{
 		txtFlightDate = new JTextField(10);
 		txtFlightTime = new JTextField(10);
@@ -44,19 +38,13 @@ public class FlightGUI extends JFrame {
 		txtFlightDest = new JTextField(10);
 		txtFlightNum = new JTextField(10);
 		txtFlightDur = new JTextField(10);
-		txtPassName = new JTextField(10);
-		txtPassLName = new JTextField(10);
-		txtPassBday = new JTextField(10);
 		lblFlightDate = new JLabel(" Date:");
 		lblFlightTime = new JLabel(" Time:");
 		lblFlightSrc = new JLabel(" Source:");
 		lblFlightDest = new JLabel(" Destination:");
 		lblFlightNum = new JLabel(" Number:");
 		lblFlightDur = new JLabel(" Duration:");
-		lblPassName = new JLabel(" First Name:");
-		lblPassLName = new JLabel(" Last Name:");
-		lblPassBday = new JLabel(" Date of Birth:");
-		btnFlightBook = new JButton("Book Flight");
+		btnAddFlight = new JButton("Add Flight");
 		btnCancel = new JButton("Cancel");
 		JPanel pnlInput = new JPanel(new GridBagLayout());
 		JPanel pnlBtns = new JPanel();
@@ -72,14 +60,11 @@ public class FlightGUI extends JFrame {
 		lblFlightDest.setLabelFor(txtFlightDest);
 		lblFlightNum.setLabelFor(txtFlightNum);
 		lblFlightDur.setLabelFor(txtFlightDur);
-		lblPassName.setLabelFor(txtPassName);
-		lblPassLName.setLabelFor(txtPassLName);
-		lblPassBday.setLabelFor(txtPassBday);
 		
-		gC.gridx = 1;
+		gC.gridx = 0;
 		gC.gridy = 0;
-		gC.gridwidth = 2;
-		pnlInput.add(new Label("Flight Information:"),gC);
+		gC.gridwidth = 4;
+		pnlInput.add(new Label("Add Flight - Enter Flight Information:"),gC);
 		gC.gridx = 0;
 		gC.gridy = 1;
 		gC.gridwidth = 1;
@@ -117,39 +102,15 @@ public class FlightGUI extends JFrame {
 		gC.gridx = 3;
 		gC.gridy = 3;
 		pnlInput.add(txtFlightDur,gC);
-		gC.insets = new Insets(20,0,0,0);
-		gC.gridx = 1;
-		gC.gridy = 4;
-		gC.gridwidth = 2;
-		pnlInput.add(new Label("Passenger Information:"),gC);
-		gC.insets = new Insets(0,0,0,0);
-		gC.gridx = 0;
-		gC.gridy = 5;
-		gC.gridwidth = 1;
-		pnlInput.add(lblPassName,gC);
-		gC.gridx = 1;
-		gC.gridy = 5;
-		pnlInput.add(txtPassName,gC);
-		gC.gridx = 2;
-		gC.gridy = 5;
-		pnlInput.add(lblPassLName,gC);
-		gC.gridx = 3;
-		gC.gridy = 5;
-		pnlInput.add(txtPassLName,gC);
-		gC.gridx = 0;
-		gC.gridy = 6;
-		pnlInput.add(lblPassBday,gC);
-		gC.gridx = 1;
-		gC.gridy = 6;
-		pnlInput.add(txtPassBday,gC);
+
 		
-		pnlBtns.add(btnFlightBook);
+		pnlBtns.add(btnAddFlight);
 		pnlBtns.add(btnCancel);
 		
 		c.add(pnlInput, BorderLayout.CENTER);
 		c.add(pnlBtns, BorderLayout.SOUTH);
 		c.setBounds(getBounds());
-		this.setPreferredSize(new Dimension(400, 250));
+		this.setPreferredSize(new Dimension(400, 200));
 		this.pack();
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
@@ -158,7 +119,7 @@ public class FlightGUI extends JFrame {
 	//main method for testing purposes:
 	public static void main(String[] args)
 	{
-		JFrame f = new FlightGUI();
+		JFrame f = new AddFlightGUI();
 	    f.setVisible(true);
 	    
 		
